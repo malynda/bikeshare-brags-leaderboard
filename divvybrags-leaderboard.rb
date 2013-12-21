f0464a3892f4c819f37c9bdc0ea990f02a4ec0a6
alexsoble <asoble@gmail.com>
2013-12-21 17:43:43 -0600

    Fix to cross-domain problem

--- divvybrags-leaderboard.rb
+++ divvybrags-leaderboard.rb
@@ -12,6 +12,7 @@ class LeaderboardPost
 end
 
 get "/entries.json" do
+  # check to see if sinatra has some built-in json tools you should use
   "{ 1 : #{LeaderboardPost.get(1).name} }"
 end
 
@@ -24,7 +25,9 @@ end
 
 post '/new_entry' do
   new_post = LeaderboardPost.new
-  new_post.name = "Alex"
-  new_post.miles = 400
+  # check first to see if the name is already in the database. if so, update miles.
+  new_post.name = params[:name]
+  new_post.miles = params[:miles]
   new_post.save
+  # send back the entire list so the sidebar can update the leaderboard! 
 end
