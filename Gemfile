6a8606550b1131011fb8cba6a953659aaacef5d9
alexsoble <asoble@gmail.com>
2013-12-23 12:16:32 -0600

    json json workin sinatra json

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 ruby "1.9.3"
 
 gem "sinatra"
+gem "sinatra-contrib"
 gem "data_mapper"
 gem "pg"
 gem "json"
--- divvybrags-leaderboard.rb
+++ divvybrags-leaderboard.rb
@@ -3,7 +3,7 @@ require "sinatra"
 require "data_mapper"
 require "pg"
 require "dm-postgres-adapter"
-require "json"
+require "sinatra/json"
 
 DataMapper.setup(:default, ENV['HEROKU_POSTGRESQL_TEAL_URL'] || 'postgres://localhost/leaderboard')
 
@@ -22,8 +22,7 @@ LeaderboardPost.create(
 )
 
 get "/entries.json" do
-  content_type :json
-  { "1" : LeaderboardPost.get(1).name }.to_json
+  json 1 => { :name => LeaderboardPost.get(1).name, :miles => LeaderboardPost.get(1).miles }
 end
 
 post '/new_entry' do
@@ -32,7 +31,3 @@ post '/new_entry' do
   new_post.save
   # send back the entire list so the sidebar can update the leaderboard! 
 end
-
-get "/" do
-  "Hello, world!"
-end 
