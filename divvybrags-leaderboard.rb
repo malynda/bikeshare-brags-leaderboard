2604cde18eddeaee80e74b6d100f6b9e79798958
alexsoble <asoble@gmail.com>
2013-12-23 11:43:43 -0600

    Databasin

--- divvybrags-leaderboard.rb
+++ divvybrags-leaderboard.rb
@@ -4,7 +4,7 @@ require "data_mapper"
 require "pg"
 require "dm-postgres-adapter"
 
-DataMapper.setup(:default, 'postgres://localhost/leaderboard')
+DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/leaderboard')
 
 class LeaderboardPost
   include DataMapper::Resource
