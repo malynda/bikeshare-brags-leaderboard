0a54fe230f1764956ae02106086b5ccec2d068a1
alexsoble <asoble@gmail.com>
2013-12-23 11:52:18 -0600

    Doin some stuff with data_mapper and json to make this thing do what it's supposed to do, you know

--- Gemfile
+++ Gemfile
@@ -4,5 +4,6 @@ ruby "1.9.3"
 gem "sinatra"
 gem "data_mapper"
 gem "pg"
-gem 'dm-postgres-adapter', :group => :production
-gem 'dm-sqlite-adapter', :group => :development
+gem "json"
+gem "dm-postgres-adapter", :group => :production
+gem "dm-sqlite-adapter", :group => :development
--- divvybrags-leaderboard.rb
+++ divvybrags-leaderboard.rb
@@ -3,6 +3,7 @@ require "sinatra"
 require "data_mapper"
 require "pg"
 require "dm-postgres-adapter"
+require "json"
 
 DataMapper.setup(:default, ENV['HEROKU_POSTGRESQL_TEAL_URL'] || 'postgres://localhost/leaderboard')
 
@@ -21,14 +22,12 @@ LeaderboardPost.create(
 )
 
 get "/entries.json" do
-  # check to see if sinatra has some built-in json tools you should use
-  "{ 1 : #{LeaderboardPost.get(1).name} }"
+  content_type :json
+  { 1 : LeaderboardPost.get(1).name }.to_json
 end
 
 post '/new_entry' do
-  new_post = LeaderboardPost.new
-  # check first to see if the name is already in the database. if so, update miles.
-  new_post.name = params[:name]
+  new_post = LeaderboardPost.first_or_create(:name => params[:name])
   new_post.miles = params[:miles]
   new_post.save
   # send back the entire list so the sidebar can update the leaderboard! 
