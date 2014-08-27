d7767992b7b229d6f56e88c9d5d9b6b4b1222fca
alexsoble <asoble@gmail.com>
2014-08-26 20:47:41 -0400

    Cleaned up some baroque code for rendering leaderboard as html

--- divvybrags-leaderboard.rb
+++ divvybrags-leaderboard.rb
@@ -53,73 +53,29 @@ get "/entries/:city/:timeperiod/" do          # HTML output for the static site 
 
   sinatra_html = '<link rel="stylesheet" href="/assets/main.css">'
 
-  if params[:timeperiod] == "alltime" 
-
-    # Return the all-time highest biking totals.
-    # With the Alta website change we need a better way to let users uniquely identify. 
-
-    ranking_hash = {}  # initialize
-
-    leaderboard_by_name = LeaderboardPost.all(city: params_city, :month.not => nil).group_by { |p| p.name }  
-    top_bike_sharers = leaderboard_by_name.keys
-    top_bike_sharers.each do |bikesharer|
-      this_persons_posts = leaderboard_by_name[bikesharer]
-      this_persons_miles = 0  # initialize
-      this_persons_posts.each do |p|
-        this_persons_miles += p.miles
+  @leaderboard = LeaderboardPost.all(:order => [:miles.desc], city: params[:city])
+  @leaderboard_json = []
+  month_names =  ["December", "November", "October", "September", "August", "July", "June", "May", "April", "March", "February", "January"] 
+  years = [2015, 2014, 2013]
+  @leaderboard_ranking = []
+  years.each do |y|
+    month_names.each do |m|
+      if params[:timeperiod] == "monthly" 
+        month_html = "<h5>" + m + "</h5><br/>"
+        month_posts = LeaderboardPost.all(month: m, year: y, order: [:miles.desc])
+        month_ranking, n = "", 1
+        if month_posts.length > 0
+          sinatra_html += "<h5>" + m + " " + y.to_s + "</h5><br/>"
+          month_posts.each do |p|
+            month_ranking += "<h10>" + n.to_s + ". " + p.name.to_s + ": " + p.miles.to_s + "mi</h10><br/>"
+            n += 1
+          end
+          sinatra_html += month_ranking
+        end        
       end
-      ranking_hash[bikesharer] = this_persons_miles
     end
-
-    leaderboard_hash = ranking_hash.sort_by{|key, value| value}.reverse
-
-    n = 1 
-    leaderboard_hash.each do |p|
-      name = p[0]
-      miles = p[1].to_s
-      post_html = n.to_s + ". " + name + ": " + miles + "mi<br>"
-      sinatra_html += post_html
-      n += 1
-    end
-
   end
-
-  if params[:timeperiod] == "monthly"     # Return bike rankings by month
-
-    leaderboard, post_html = [], ""
-    month_names =  ["December", "November", "October", "September", "August", "July", "June", "May", "April", "March", "February", "January"] 
-    years = [2015, 2014, 2013]
-    leaderboard_ranking = []
-    years.each do |y|
-      month_names.each do |m|
-        month_posts = LeaderboardPost.all(city: params_city, month: m, year: y, order: [:miles.desc])
-        month_ranking, n = [], 1
-        month_posts.each do |p|
-          month_ranking << { n => { name: p.name, miles: p.miles } }
-          n +=1
-        end
-        if month_ranking.length > 0
-          leaderboard << { "#{m} #{y}" => month_ranking }
-        end
-      end
-    end
-
-    leaderboard.each do |m|
-      month_name = m.keys[0]
-      month_html = "<h5>" + month_name + "</h5><br/>";
-      this_month_rankings = m[month_name]
-      this_month_rankings.each do |ranking|
-        rank_number = ranking.keys[0]
-        rank_holder_deatails = ranking[rank_number]
-        name = rank_holder_deatails[:name]
-        miles = rank_holder_deatails[:miles]
-        month_html += "<h10>" + rank_number.to_s + ". " + name + ": " + miles.to_s + "mi</h10><br/>";
-      end
-      sinatra_html += month_html
-    end
-
-  end 
-
+  
   sinatra_html
 
 end
