2ac974a0d2e21f5503dde502b6a223fca50d11a1
malynda <mchizek@gmail.com>
2015-10-04 16:46:02 -0400

    more

--- cabibrags-leaderboard.rb
+++ cabibrags-leaderboard.rb
@@ -33,14 +33,14 @@ end
 
 def render_leaderboard_json
 
-  @leaderboard = LeaderboardPost.all(:order => [:miles.desc], city: params[:city])
-  @leaderboard_json, n = [], 1
-  @the_posts = weed_out_duplicates_and_resort(@leaderboard)
-  @the_posts.each do |p|
-    @leaderboard_json << { n => { name: p.name, miles: p.miles } }
+  leaderboard = LeaderboardPost.all(:order => [:miles.desc], city: params[:city])
+  leaderboard_json, n = [], 1
+  the_posts = weed_out_duplicates_and_resort(leaderboard)
+  the_posts.each do |p|
+    leaderboard_json << { n => { name: p.name, miles: p.miles } }
     n += 1
   end
-  json @leaderboard_json
+  json leaderboard_json
 end
 
 get "/entries.json" do            # JSON output for the Chrome extensions to consume
