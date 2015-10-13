2d84a2ac3ec43c0683e592c4b1477c512f2fcfea
malynda <mchizek@gmail.com>
2015-10-13 18:16:00 -0400

    working on render_leaderboard_json

--- cabibrags-leaderboard.rb
+++ cabibrags-leaderboard.rb
@@ -21,7 +21,7 @@ end
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
-def weed_out_duplicates_and_resort(posts)
+def weed_out_duplicates_and_resort(*posts)
   posts.group_by { |p| p.name }
     .sort_by { |name, posts| posts.max {|a,b| a.miles } }              # Sort any duplicate legacy posts for highest milage
     .map { |name, posts| posts[0] }                                    # Select highest milage post, weeding out duplicates
@@ -32,7 +32,7 @@ def render_leaderboard_json
   @leaderboard = LeaderboardPost.all(:order => [:miles.desc], city: params[:city])
   # print @leaderboard
   @leaderboard_json, n = [], 1
-  # @the_posts = weed_out_duplicates_and_resort(@leaderboard)
+  @the_posts = weed_out_duplicates_and_resort(*@leaderboard)
   @leaderboard.each do |p|
     @leaderboard_json << { n => { name: p.name, miles: p.miles } }
     n += 1
